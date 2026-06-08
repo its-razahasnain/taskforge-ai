f5a7153b54743a93b3b0cd63411f14377d75b474
Hasnain Raza <razahasnain.work@gmail.com>
2026-06-09 02:40:19 +0500

    Refactor task addition process and enhance menu display

---
+++ helper.py
@@ -0,0 +1,10 @@
+def print_equalto_seperator(n):
+    print("=" * n)
+
+
+def print_dash_seperator(n):
+    print("-" * n)
+
+
+def print_new_line():
+    print("\n")
--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+from helper import print_equalto_seperator, print_new_line, print_dash_seperator
+
 
 task_list = []
 
+
 def add_task():
     while True:
-        task = input("What task would you like to schedule?").strip()
+        print_new_line()
+        task = input("What task would you like to schedule: ").strip()
         if task:
             task_list.append(task)
-            print("Task successfully added to your manager!")
+            print_new_line()
+            print_dash_seperator(40)
+            print(f"{task} successfully added to your manager!")
+            print_dash_seperator(40)
             break
+        print_new_line()
+        print_dash_seperator(26)
         print("Task name cannot be empty!")
+        print_dash_seperator(26)
+
+
+def show_menu():
+    menu_option = ["Add Task", "View Task", "Exit"]
+    print_dash_seperator(39)
+    print("Welcome to Taskforge AI - Task Manager")
+    print_dash_seperator(39)
+    print("Menu Options:")
+    for i, option in enumerate(menu_option, start=1):
+        print(f"{i}. {option}")
+
 
 def main():
-    print("Welcome to Taskforge AI")
+    show_menu()
     add_task()
 
 

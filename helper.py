d90b55056ecaab02c8fef82081e9d60bc22a7087
Hasnain Raza <razahasnain.work@gmail.com>
2026-06-09 05:51:22 +0500

    Add show_message function to format and display messages with separators

--- helper.py
+++ helper.py
@@ -12,3 +12,11 @@ def print_new_line():
 
 def task_len(task):
     return len(task) + 20
+
+
+def show_message(message):
+    print_new_line()
+    print_dash_seperator(30)
+    print(message)
+    print_dash_seperator(30)
+    print_new_line()
